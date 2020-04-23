147e290bbf9782b03567f00e808df9f7110e633f
BraindeadBZH <braindeadbzh@gmail.com>
2020-04-23 14:08:04 +0200

    Added displaying card details in database edit dialog.

--- addons/cardengine/ui/dialogs/edit_database_dialog/edit_database_dialog.gd
+++ addons/cardengine/ui/dialogs/edit_database_dialog/edit_database_dialog.gd
@@ -1,12 +1,14 @@
 tool
 extends WindowDialog
 
-var _db: String = ""
+var _db: CardDatabase = null
 
 func set_database(id: String):
 	_clear_lists()
-	_db = id
-	var cards = CardEngine.db().get_database(id).cards()
+	
+	_db = CardEngine.db().get_database(id)
+	
+	var cards = _db.cards()
 	for card_id in cards:
 		var card = cards[card_id]
 		$MainLayout/CardsLayout/CardList.add_item(card.id)
@@ -20,3 +22,25 @@ func _on_DoneBtn_pressed():
 
 func _on_EditDatabaseDialog_about_to_show():
 	_clear_lists()
+
+func _on_CardList_item_selected(index):
+	var list = $MainLayout/CardsLayout/DetailsLayout/DetailsList
+	list.clear()
+	
+	var id = $MainLayout/CardsLayout/CardList.get_item_text(index)
+	var card = _db.get_card(id)
+	
+	list.add_item("Categories:")
+	var categs = card.categories()
+	for categ in categs:
+		list.add_item("  * %s: %s" % [categ, categs[categ]])
+		
+	list.add_item("Values:")
+	var values = card.values()
+	for value in values:
+		list.add_item("  * %s = %f" % [value, values[value]])
+		
+	list.add_item("Texts:")
+	var texts = card.texts()
+	for text in texts:
+		list.add_item("  * %s: %s" % [text, texts[text]])
