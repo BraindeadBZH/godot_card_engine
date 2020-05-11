86a1eea4ddf6258d39b58c5d7d5d78a834cfb04d
BraindeadBZH <braindeadbzh@gmail.com>
2020-05-11 16:47:15 +0200

    Added visual info to container.

--- addons/cardengine/ui/dialogs/new_container_dialog/new_container_dialog.gd
+++ addons/cardengine/ui/dialogs/new_container_dialog/new_container_dialog.gd
@@ -3,6 +3,8 @@ extends AbstractFormDialog
 
 onready var _cont_id = $MainLayout/Form/ContainerId
 onready var _cont_name = $MainLayout/Form/ContainerName
+onready var _cont_visual = $MainLayout/Form/VisualLayout/ContainerVisual
+onready var _select_dlg = $SelectDlg
 
 func _ready():
 	setup("new_container", CardEngine.cont())
@@ -23,3 +25,9 @@ func _fill_form(data: Dictionary):
 	_cont_id.editable = false
 	_cont_id.text = data["id"]
 	_cont_name.text = data["name"]
+
+func _on_SelectBtn_pressed():
+	_select_dlg.popup_centered_minsize(Vector2(400, 250))
+
+func _on_SelectDlg_file_selected(path):
+	_cont_visual.text = path
