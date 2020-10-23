d2ebe6e13793d78bfd09b411af3ab1da7339f7d6
BraindeadBZH <braindeadbzh@gmail.com>
2020-10-23 17:25:27 +0200

    Fixed missing file

---
+++ addons/cardengine/card/card_transitions.gd
@@ -0,0 +1,10 @@
+class_name CardTransitions
+extends Reference
+
+class OrderTransition:
+	var duration: float = 0.0
+	var type = Tween.TRANS_LINEAR
+	var easing = Tween.EASE_IN
+
+
+var order: OrderTransition = OrderTransition.new()
