395db583fe2f9ff56f421e4e1d6642c873bccfb4
BraindeadBZH <braindeadbzh@gmail.com>
2020-10-28 16:29:33 +0100

    Changed flip animation to be more satisfying.

--- _private/containers/card_list/card_list_private.gd
+++ _private/containers/card_list/card_list_private.gd
@@ -53,8 +53,8 @@ func _ready():
 	_transitions.out_anchor.type = Tween.TRANS_QUAD
 	_transitions.out_anchor.easing = Tween.EASE_IN
 
-	_transitions.flip_start.duration = 0.3
-	_transitions.flip_start.type = Tween.TRANS_EXPO
+	_transitions.flip_start.duration = 0.2
+	_transitions.flip_start.type = Tween.TRANS_BACK
 	_transitions.flip_start.easing = Tween.EASE_IN
 
 	_transitions.flip_end.duration = 0.3
