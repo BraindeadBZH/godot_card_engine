445bcb0d367a51fac51d6fafe4463b25ef6c007b
BraindeadBZH <braindeadbzh@gmail.com>
2020-12-26 14:42:02 +0100

    Fixed animation preview not working.

--- addons/cardengine/ui/dialogs/anim_preview_dialog/anim_preview_dialog.gd
+++ addons/cardengine/ui/dialogs/anim_preview_dialog/anim_preview_dialog.gd
@@ -15,6 +15,7 @@ func show_anim(anim: AnimationData) -> void:
 func _on_AnimPreviewDialog_about_to_show() -> void:
 	_preview_card.set_root_trans_immediate(CardTransform.new())
 	_preview_card.set_animation(_anim)
+	_preview_card.set_instance(CardInstance.new(CardData.new("preview")))
 
 
 func _on_AnimPreviewDialog_popup_hide() -> void:
