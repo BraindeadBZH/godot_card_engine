ab3dacf4a5f7202a7d0ba6690a3935bd16036c6f
BraindeadBZH <braindeadbzh@gmail.com>
2021-01-04 11:32:33 +0100

    Minor tweak.

--- effects/mana_double.gd
+++ effects/mana_double.gd
@@ -8,11 +8,7 @@ func _init(id: String, name: String).(id, name) -> void:
 # Override this to limit the affected cards or leave null to affect all the cards
 func get_filter() -> Query:
 	var filter := Query.new()
-	
-	# warning-ignore:return_value_discarded
-	filter.from(["rarity:common"])
-	
-	return filter
+	return filter.from(["rarity:common"])
 
 
 # Override this to returns an array of modifiers applied by this effect
