9a10b00b22d267cceaac428fc30c14c00bad9fdc
BraindeadBZH <braindeadbzh@gmail.com>
2021-10-11 19:18:27 +0200

    Changed menu buttons to use theme for better compatibility with Godot 4.

--- widgets/menu_button/menu_button.gd
+++ widgets/menu_button/menu_button.gd
@@ -1,16 +1,2 @@
 @tool
-extends TextureButton
-
-@export var button_text = "BUTTON":
-	set(new_text):
-		button_text = new_text
-		if !is_inside_tree():
-			return # Avoid crash when value is set before the node is added to the tree
-		_text.text = new_text
-
-
-@onready var _text = $Text
-
-
-func _ready():
-	_text.text = button_text # Compensate for not being able to set the value before in the tree
+extends Button
